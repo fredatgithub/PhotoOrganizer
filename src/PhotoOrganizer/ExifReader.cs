5d599069574950fc112e7d294d19fac2abbc93e3
Jonas Samuelsson <jonas@samuelsson.at>
2014-12-13 21:10:11 +0100

    ExifReader draft

---
+++ src/PhotoOrganizer/ExifReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PhotoOrganizer
+{
+   public class ExifReader
+   {
+      public IEnumerable<ExifFile> Scan(string directory)
+      {
+         return from filePath in Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories)
+                let file = new FileInfo(filePath)
+                select new ExifFile
+                {
+                   CapturedDate = file.LastWriteTime,
+                   Path = filePath,
+                   Size = (int)file.Length
+                };
+      }
+
+      public class ExifFile
+      {
+         public string Path { get; set; }
+         public DateTime CapturedDate { get; set; }
+         public int Size { get; set; }
+      }
+   }
+}
